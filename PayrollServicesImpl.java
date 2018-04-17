3edcc2972833cb78cec476a20e2965e240b9fda0
tejaswini2909 <38201412+tejaswini2909@users.noreply.github.com>
2018-04-17 17:25:46 +0530

    Add files via upload

---
+++ PayrollServicesImpl.java
@@ -0,0 +1,18 @@
+package com.cg.payroll.services;
+import com.cg.payroll.beans.Associate;
+public class PayrollServicesImpl {
+			public int acceptAssociateDetails(String firstName,String lastName,String emailId,String department,
+					String designation,String pancard,int yearlyInvestmentUnder80C,int basicSalary,int epf, 
+					 int companyPf,int accountNo,String bankName,String ifscCode) {
+			return 0;
+			}
+		public int calculateNetSalary(int associateId) {
+			return 0;
+		}
+		public Associate getAssociateDetails(int associateId) {
+			return null;
+		}
+		public Associate[] getAllAssociatesDetails() {
+			return null;
+			}
+}
