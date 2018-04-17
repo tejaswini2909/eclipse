3c64e85955c330a8ec4dbda5733744e655d6d196
tejaswini2909 <38201412+tejaswini2909@users.noreply.github.com>
2018-04-17 17:08:02 +0530

    Add files via upload

---
+++ PayrollDAOServicesImpl.java
@@ -0,0 +1,19 @@
+package com.cg.payroll.daoservices;
+import com.cg.payroll.beans.Associate;
+public class PayrollDAOServicesImpl {
+	int insertAssociate(Associate associate){
+		return 0;
+	}
+	boolean updateAssociate(Associate associate) {
+		return true;
+	}
+	boolean deleteAssociate(int associateId) {
+		return false;
+	}
+	Associate getAssociate(int associateId) {
+		return null;
+	}
+	Associate[] getAssociates() {
+		return null;
+	}
+}
